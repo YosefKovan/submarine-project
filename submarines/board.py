1566c736dc97e2de1d31d1082225e19b111bea15
Yossi Kovan <yossikovan@gmail.com>
2025-10-27 15:14:56 +0200

    adding the in bounds function and the create matrix function

--- submarines/board.py
+++ submarines/board.py
@@ -1,25 +1,44 @@
 
-def create_matrix(size, fill=0):
+#===============================
+#         create matrix
+#===============================
+def create_matrix(size, fill):
     """this function will create a matrix - default value is 0"""
 
     #using list comprehension
     return [fill for j in range(size) for i in range(size)]
 
+#===============================
+#           in bounds
+#===============================
+def in_bounds(n, x, y):
+
+     return 0 < x < n and 0 < y < n
+
+#===============================
+#     count remaining ships
+#===============================
+def count_remaining_ships(ships, shots):
+
+    count = 0
+
+    for i in range(len(ships)):
+        for j in range(len(ships[i])):
+            if ships[i][j] == 1 and shots[i][j] == False:
+                count += 1
+
+    return count
 
 
-
-def create_bool_matrix():
-    pass
-
-def in_bounds():
-    pass
-
-def count_remaining_ships():
-    pass
-
+#===============================
+#         render public
+#===============================
 def render_public():
     pass
 
+#===============================
+#         render reveal
+#===============================
 def render_reveal():
     pass
 
--- submarines/game.py
+++ submarines/game.py
@@ -1,7 +1,7 @@
 from submarines.board import create_matrix
 
 def init_game(size, ships, max_shots):
-
+    pass
 
 
 
